5db6675685fb553c1a4e0971f687742104d6bab8
Rustam Kovhaev <rkovhaev@gmail.com>
2020-07-27 23:42:17 -0700

    usb: hso: check for return value in hso_serial_common_create()
    [ Upstream commit e911e99a0770f760377c263bc7bac1b1593c6147 ]
    
    in case of an error tty_register_device_attr() returns ERR_PTR(),
    add IS_ERR() check
    
    Reported-and-tested-by: syzbot+67b2bd0e34f952d0321e@syzkaller.appspotmail.com
    Link: https://syzkaller.appspot.com/bug?extid=67b2bd0e34f952d0321e
    Signed-off-by: Rustam Kovhaev <rkovhaev@gmail.com>
    Reviewed-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/net/usb/hso.c
+++ drivers/net/usb/hso.c
@@ -2269,17 +2269,16 @@ static int hso_serial_common_create(struct hso_serial *serial, int num_urbs,
 
 	tty_port_init(&serial->port);
 
-	if (obtain_minor(serial))
+	minor = get_free_serial_index();
+	if (minor < 0)
 		goto exit2;
 
 	/* register our minor number */
 	serial->parent->dev = tty_port_register_device_attr(&serial->port,
 			tty_drv, serial->minor, &serial->parent->interface->dev,
 			serial->parent, hso_serial_dev_groups);
-	if (IS_ERR(serial->parent->dev)) {
-		release_minor(serial);
+	if (IS_ERR(serial->parent->dev))
 		goto exit2;
-	}
 	dev = serial->parent->dev;
 
 	serial->magic = HSO_SERIAL_MAGIC;
