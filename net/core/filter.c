0f896ab663e5a854edd8213123ff41f3a9689cac
ahnet-69 <syedmusicrealise@gmail.com>
2024-09-05 07:36:44 +0000

    a32: Fixup of left over conflicts

--- net/core/filter.c
+++ net/core/filter.c
@@ -2166,8 +2166,6 @@ static int bpf_skb_proto_6_to_4(struct sk_buff *skb)
 			shinfo->gso_type |=  SKB_GSO_TCPV4;
 		}
 
-		/* Due to IPv4 header, MSS can be upgraded. */
-		skb_increase_gso_size(shinfo, len_diff);
 		/* Header must be checked, and gso_segs recomputed. */
 		shinfo->gso_type |= SKB_GSO_DODGY;
 		shinfo->gso_segs = 0;
